f6a8011a5a0508252ba913b24c9ba6f4c6cf9b9c
Aljunun <recepsavas001@gmail.com>
2024-06-10 07:28:01 +0300

    fix carousel

--- src/Components/ecobord/Ecobord.jsx
+++ src/Components/ecobord/Ecobord.jsx
@@ -22,9 +22,9 @@ function Ecobord() {
                             <div className="carousel-item position-relative active" style={{ height: '430px' }}>
                                 <img className=" " src="image/img4.png" style={{ objectFit: 'cover' }} alt="Slide 1" />
                                 <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
-                                    <div className="p-3" style={{ maxWidth: '700px' }}>
+                                    <div className="p-3" style={{ maxWidth: '1000px' }}>
                                         <h1 className="display-4 text-white mb-3 animate__animated animate__fadeInDown">Kompozit Üretim</h1>
-                                        <p className="mx-md-5 px-5 animate__animated animate__bounceIn"> Roket, İHA ve özel parçalarınızı kaliteden ödün vermeden avantajlı fiyatlarla ürettirin. Uzman ekibimizle, en zorlu tasarımlarınızı hayata geçirin ve projelerinizi bir adım öne taşıyın.  </p>
+                                        <p className="mx-md-5   animate__animated animate__bounceIn"> Roket, İHA ve özel parçalarınızı kaliteden ödün vermeden avantajlı fiyatlarla ürettirin. Uzman ekibimizle, en zorlu tasarımlarınızı hayata geçirin ve projelerinizi bir adım öne taşıyın.  </p>
                                         <Link to={'/Kompozit'}>  <a className="btn btn-outline-light py-2 px-4 mt-3 animate__animated animate__fadeInUp" href="#">İncele</a></Link>
                                     </div>
                                 </div>
@@ -32,9 +32,9 @@ function Ecobord() {
                             <div className="carousel-item position-relative" style={{ height: '430px' }}>
                                 <img className=" " src="image/img5.png" style={{ objectFit: 'cover' }} alt="Slide 2" />
                                 <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
-                                    <div className="p-3" style={{ maxWidth: '700px' }}>
+                                    <div className="p-3" style={{ maxWidth: '1000px' }}>
                                         <h1 className="display-4 text-white mb-3 animate__animated animate__fadeInDown">Uçuş Malzemeleri</h1>
-                                        <p className="mx-md-5 px-5 animate__animated animate__bounceIn"> Her projenizin ihtiyaçlarını karşılayacak yüksek kaliteli parçaları kolayca ve hızlı bir şekilde temin edin. Geniş ürün yelpazemiz sayesinde projeleriniz için en uygun çözümleri bulabilirsiniz. </p>
+                                        <p className="mx-md-5   animate__animated animate__bounceIn"> Her projenizin ihtiyaçlarını karşılayacak yüksek kaliteli parçaları kolayca ve hızlı bir şekilde temin edin. Geniş ürün yelpazemiz sayesinde projeleriniz için en uygun çözümleri bulabilirsiniz. </p>
                                        <Link to={'/Malzeme'}><a className="btn btn-outline-light py-2 px-4 mt-3 animate__animated animate__fadeInUp" href="#">İncele</a></Link> 
                                     </div>
                                 </div>
@@ -42,9 +42,9 @@ function Ecobord() {
                             <div className="carousel-item position-relative" style={{ height: '430px' }}>
                                 <img className=" " src="image/img8.png" style={{ objectFit: 'cover' ,filter:'brightness(50%)' }} alt="Slide 3" />
                                 <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
-                                    <div className="p-3" style={{ maxWidth: '700px' }}>
+                                    <div className="p-3" style={{ maxWidth: '1000px' }}>
                                         <h1 className="display-4 text-white mb-3 animate__animated animate__fadeInDown">Aviyonik Kitler </h1>
-                                        <p className="mx-md-5 px-5 animate__animated animate__bounceIn">Projelerinizde güvenle kullanabileceğiniz yüksek kaliteli aviyonik kitler sunuyoruz. Güvenilir, hassas ve performans odaklı çözümlerimizle uçuş güvenliğinizi ve verimliliğinizi artırın.</p>
+                                        <p className="mx-md-5  animate__animated animate__bounceIn">Projelerinizde güvenle kullanabileceğiniz yüksek kaliteli aviyonik kitler sunuyoruz. Güvenilir, hassas ve performans odaklı çözümlerimizle uçuş güvenliğinizi ve verimliliğinizi artırın.</p>
                                         <Link to={'/Aviyonik'}>    <a className="btn btn-outline-light py-2 px-4 mt-3 animate__animated animate__fadeInUp" href="#">İncele</a> </Link>
                                     </div>
                                 </div>
