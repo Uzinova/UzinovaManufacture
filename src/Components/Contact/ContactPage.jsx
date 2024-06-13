3eaf603c54e9ce1692856f21661214ccd6e68b49
Aljunun <recepsavas001@gmail.com>
2024-06-13 15:15:09 +0300

    UpdaeIletisim

--- src/Components/Contact/ContactPage.jsx
+++ src/Components/Contact/ContactPage.jsx
@@ -8,9 +8,9 @@ const ContactPage = () => {
     <div className="contact-page">
       <div className="contact-info">
         <h2>İletişim Bilgileri</h2>
-        <p>Adres: 1234 Örnek Sokak, İstanbul, Türkiye</p>
-        <p>Telefon: +90 123 456 78 90</p>
-        <p>E-posta: info@company.com</p>
+        <p>Adres: Kemalpaşa, Güldeste Sk. No:3, 54000 Serdivan/Sakarya        </p>
+        <p>Telefon: +90 536 582 19 02</p>
+        <p>E-posta: uzinovas@gmail.com</p>
         {/* Harita veya diğer bileşenler buraya eklenebilir */}
       </div>
       <ContactForm />
